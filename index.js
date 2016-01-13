cd6b3bed349ae271244de59b3a1eed47054016b2
EGOIST <0x142857@gmail.com>
2016-01-13 15:10:27 +0800

    remove assign

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
+function setDefaultValue(obj, value) {
+	return obj === undefined? value : obj
+}
+
 module.exports = function safetify(config) {
 	if (typeof config !== 'object') {
 		throw new TypeError('Expect an object but got ' + typeof config)
 	}
-	return Object.assign({}, config, {
-		resolve: {
-			extenstions: ['', '.js']
-		},
-		resolveLoader: {
-			modulesDirectories: []
-		},
-		module: {
-			loaders: []
-		},
-		plugins: []
-	})
+	config.resolve = setDefaultValue(config.resolve, {})
+	config.resolve.extenstions = setDefaultValue(config.resolve.extenstions, ['', '.js'])
+
+	config.resolveLoader = setDefaultValue(config.resolveLoader, {})
+	config.resolveLoader.modulesDirectories = setDefaultValue(config.resolveLoader.modulesDirectories, [])
+
+	config.module = setDefaultValue(config.module, {})
+	config.module.loaders = setDefaultValue(config.module.loaders, [])
+
+	config.plugins = setDefaultValue(config.plugins, [])
+
+	return config
 }
