ea36fe47386418844fd43e35e35a6a0f5343a0c2
EGOIST <0x142857@gmail.com>
2016-01-13 14:15:15 +0800

    use tab

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ module.exports = function safetify(config) {
 	if (typeof config !== 'object') {
 		throw new TypeError('Expect an object but got ' + typeof config)
 	}
-  return Object.assign({}, config, {
-    resolve: {
-      extenstions: ['', '.js']
-    },
-    resolveLoader: {
-      modulesDirectories: []
-    },
-    module: {
+	return Object.assign({}, config, {
+		resolve: {
+			extenstions: ['', '.js']
+		},
+		resolveLoader: {
+			modulesDirectories: []
+		},
+		module: {
 			loaders: []
-    },
+		},
 		plugins: []
-  })
+	})
 }
